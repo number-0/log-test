f5084b1f5af8111e71e3d6e0ddccaca6ff01191d
songhengliang <xingren123>
2020-07-13 17:15:47 +0800

    init

--- log4j2-test/src/main/java/com/shl/log/log4j2/Log4j2Test.java
+++ log4j2-test/src/main/java/com/shl/log/log4j2/Log4j2Test.java
@@ -3,6 +3,7 @@ package com.shl.log.log4j2;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 
 /**
  * log4j2的日志门面使用log4j2-api
@@ -32,5 +33,26 @@ public class Log4j2Test {
 
 
     Thread.sleep(Integer.MAX_VALUE);
+  }
+
+  static {
+    System.setProperty("logging.file.path", "/Users/songhengliang/java/workspace-shl/log-test/log4j2-test/log4j2-test");
+    System.setProperty("project.name", "log4j2-test");
+    System.setProperty("logging.console.level", "info");
+  }
+
+  @Test
+  public void template() throws Exception {
+    Logger LOGGER = LoggerFactory.getLogger(Log4j2Test.class);
+
+    while(true) {
+      Thread.sleep(5L);
+      LOGGER.info("info ........");
+      LOGGER.warn("info ........");
+      LOGGER.error("info ........");
+    }
+
+
+//    Thread.sleep(Integer.MAX_VALUE);
   }
 }
